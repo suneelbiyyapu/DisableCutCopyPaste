33e533561fc95fb12489912352edace18e7c7a70
DESKTOP-KFRLMGS\SuneelPC <suneel.biyyapu@gmail.com>
2018-05-22 16:04:04 +0530

    Code for hiding Menu options.

---
+++ DisableCutCopyPaste/DisableCutCopyPaste.Android/CustomEntryRenderer.cs
@@ -0,0 +1,52 @@
+﻿using Android.Content;
+using Android.Views;
+using DisableCutCopyPaste;
+using DisableCutCopyPaste.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace DisableCutCopyPaste.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.CustomSelectionActionModeCallback = new Callback();
+            }
+        }
+    }
+
+    // Following class is for hiding Menu control options (Cut, Copy, Paste ... etc).
+    internal class Callback : Java.Lang.Object, ActionMode.ICallback
+    {
+
+        public bool OnActionItemClicked(ActionMode mode, IMenuItem item)
+        {
+            return false;
+        }
+
+        public bool OnCreateActionMode(ActionMode mode, IMenu menu)
+        {
+            return false;
+        }
+
+        public void OnDestroyActionMode(ActionMode mode)
+        {
+
+        }
+
+        public bool OnPrepareActionMode(ActionMode mode, IMenu menu)
+        {
+            return false;
+        }
+    }
+}
---
+++ DisableCutCopyPaste/DisableCutCopyPaste.iOS/CustomEntryRenderer.cs
@@ -0,0 +1,39 @@
+﻿using DisableCutCopyPaste;
+using DisableCutCopyPaste.iOS;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using ObjCRuntime;
+using Foundation;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace DisableCutCopyPaste.iOS
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Control != null)
+            {
+                // Write your logic
+            }
+        }
+
+        // Following method is for hiding Menu control options (Cut, Copy, Paste ... etc).
+        public override bool CanPerform(Selector action, NSObject withSender)
+        {
+            NSOperationQueue.MainQueue.AddOperation(() =>
+            {
+                UIMenuController.SharedMenuController.SetMenuVisible(false, false);
+            });
+
+            if (action.Name == "paste:" || action.Name == "copy:" || action.Name == "cut:")
+                return false;
+
+            return base.CanPerform(action, withSender);
+        }
+    }
+}
